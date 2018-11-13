edbd5721ed25219d7be67903665ce953ab62a116
251378436 <251378436@qq.com>
2018-11-14 00:13:12 +1300

    change to test2

--- src/components/Test/Test.vue
+++ src/components/Test/Test.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>This is the test</h1>
+        <h1>This is the test2</h1>
         <button v-on:click="test()">Test</button>
         {{moduleASysUser}} <br>
         <counter></counter>
